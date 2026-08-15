13856af1a425a9c26445f7acba8a4ea645affa45
ukankankanayato-droid <ukankankan.ayato@gmail.com>
2026-08-16 08:02:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -26,13 +26,14 @@ STOCK_DICT = {
 selected_option = st.selectbox("銘柄を選択", list(STOCK_DICT.keys()))
 symbol = STOCK_DICT[selected_option]
 
-# ID用文字列の整形（特殊文字を除去）
-container_id = f"tv_{symbol.replace(':', '_')}"
+# ID用文字列（英数字のみ）
+clean_id = symbol.replace(":", "_")
 
-# TradingView チャート埋め込み
+# TradingView チャート埋め込み（銘柄変更時に再描画される構造）
 tv_html = f"""
-<div class="tradingview-widget-container" style="height:100%;width:100%">
-  <div id="{container_id}" style="height:500px;width:100%"></div>
+<!-- {symbol} -->
+<div class="tradingview-widget-container" style="height:500px;width:100%">
+  <div id="tv_chart_{clean_id}" style="height:500px;width:100%"></div>
   <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
   <script type="text/javascript">
   new TradingView.widget({{
@@ -45,11 +46,10 @@ tv_html = f"""
     "locale": "ja",
     "enable_publishing": false,
     "allow_symbol_change": true,
-    "container_id": "{container_id}"
+    "container_id": "tv_chart_{clean_id}"
   }});
   </script>
 </div>
 """
 
-# keyを指定して銘柄変更時にコンポーネントを再作成
-components.html(tv_html, height=520, key=f"chart_{symbol}")
+components.html(tv_html, height=520)
